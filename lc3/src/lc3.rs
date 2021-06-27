a0dc9c518f86ba6aee2121f3dc5ca2e4834a18cf
Bexx Modebadze <bexxmodd@seas.upenn.edu>
2021-06-26 22:25:15 -0400

    add instruction

--- lc3/src/lc3.rs
+++ lc3/src/lc3.rs
@@ -12,6 +12,8 @@ pub enum Register {
     RCount = 10,
 }
 
+static mut REG: &'static [u16] = &[0; Register::RCount as usize];
+
 pub enum Instruction {
     OPBr = 0, // branch
     OPAdd,    // add
@@ -46,5 +48,32 @@ pub fn sign_extend(mut x: u16) -> u16 {
 }
 
 pub fn update_flags(r: u16) {
-    
+    if REG[r as usize] == 0 {
+        REG[RCond as usize] = BRz;
+    } else if REG[r as usize] >> 15 {
+        REG[RCond as usize] = BRn;
+    } else {
+        REG[RCond as usize] = BRp;
+    }
+}
+
+pub fn add_dir(instr: u16) {
+    //destination register (DR) 
+    let r0 = (instr >> 9) & 0x7;
+
+    // first operand
+    let r1 = (instr >> 6) & 0x7;
+
+    // where we are in immediate mode
+    let imm_flag = (isntr >> 5) & 0x1;
+
+    if imm_flag {
+        let imm5: u16 = sign_extend(instr & 0x1F);
+        REG[r0] = REG[r1] + imm5;
+    } else {
+        let r2: u16 = instr & 0x7;
+        REG[r0] = REG[r1] + REG[r2];
+    }
+
+    update_flags(r0);
 }
